668cda519784c215ae80c9b6bfa0fe242aceb555
rachidcooder <cooderherou@gmail.com>
2024-10-21 10:47:31 +0100

    DEV :: Empty Project

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,4 @@
 <template>
-  <div class=" bg-white ">
-    
-   <div class="flex  w-full sm:h-screen ">
-     <sections-header-menu></sections-header-menu>
-   <template v-for="i in 1">
-      <sections-products
-        :key="i"
-        :data-key="i"
-        v-if="$settings.sections[`products_${i}`].active"
-        :section="$settings.sections[`products_${i}`]"
-        
-      ></sections-products>
-    </template>
-    
-    </div>
-     
 
-<!--
-    <sections-posts v-if="$settings.sections.posts.active"></sections-posts>
-    <sections-services
-      v-if="$settings.sections.services.active"
-    ></sections-services>
-    -->
- 
-  </div>
 </template>
-<script>
-export default {
-  async fetch() {
-   // console.log(" product 1 is active ?",this.$settings.sections[`products_${1}`].active);
-    this.$store.state.seo.title = this.$settings.store_name;
-    this.$store.state.seo.description = this.$settings.store_description;
-  },
-  mounted() {
-    this.$tools.call("PAGE_VIEW");
-    this.$storeino.fbpx("PageView");
-  },
-};
-</script>
+
